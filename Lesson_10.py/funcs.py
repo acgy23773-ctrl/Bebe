21aae0d31562e17d8ad03d8465e368b28b6d9d31
acgy23773-ctrl <acgy23773@gmail.com>
2025-12-20 12:41:24 +0300

    Questions randomization + right answers count

--- Lesson_10.py/funcs.py
+++ Lesson_10.py/funcs.py
@@ -1,24 +1,28 @@
 import data
 import random
-points = 0
+
+correct_answers_count = 0
 quest_index = 0
 quests_data = data.questions
+random.shuffle(quests_data)
 
-def generate_quest(QUEST, buttons):
+def generate_quest(QUEST, buttons, info):
+    global quest_index
+    
     if quest_index < len(quests_data):
-        quest = quests_data[quest_index]["quest"]
-        QUEST.config(text=quest)
+        QUEST.config(text=quests_data[quest_index]["quest"])
+        answers = list(quests_data[quest_index]["answers"])
+        random.shuffle(answers)
         for i in range(len(buttons)):
-            buttons[i].config(text=quests_data[quest_index]["answers"][i])
+            buttons[i].config(text=answers[i], state="normal")
     else:
-        QUEST.config(text="Викторина закончена")
+        QUEST.config(text=f"Конец! {correct_answers_count} из {len(quests_data)}")
         for btn in buttons:
             btn.config(state="disabled")
 def choice(button, QUEST, buttons, info):
     global quest_index
+    global correct_answers_count
     if button["text"] == quests_data[quest_index]["right"]:
-        quest_index += 1 
-        info.config(text="")
-        generate_quest(QUEST, buttons)
-    else:
-        info.config(text="Неверно")
+        correct_answers_count += 1 
+    quest_index += 1
+    generate_quest(QUEST, buttons, info)
